094bfe9dfccb49a3566ef3119c372f1fb5cd4c82
Amar Kulovac <kulovac@ualberta.ca>
2026-02-10 10:07:08 -0700

    Added javadoc

--- code/app/src/main/java/com/example/code/City.java
+++ code/app/src/main/java/com/example/code/City.java
@@ -1,26 +1,55 @@
 package com.example.code;
 
+/**
+ * This is a class that defines a City.
+ */
 public class City {
+    /**
+     * The name of the city
+     */
     private String city;
+    /**
+     * The name of the province the city belongs to
+     */
     private String province;
 
+    /**
+     * @param city
+     *      The name of the city
+     * @param province
+     *      The name of the province
+     */
     public City(String city, String province) {
         this.city = city;
         this.province = province;
     }
 
+    /**
+     * @return the name of this city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * @param city
+     *      The name of the city to set this cities name to
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * @return the province name
+     */
     public String getProvince() {
         return province;
     }
 
+    /**
+     * @param province
+     *      The name of the province to set this city to
+     */
     public void setProvince(String province) {
         this.province = province;
     }
